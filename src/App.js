26002e5df85271e7442d286abe9c53b8a697a279
webdeveloper20 <abdulhsn.allahverdiyev@mail.ru>
2022-07-27 14:17:52 +0400

    reactproje

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+     <img src="/logo192.png"/>
+     <img src={logo}/>
+    </div>
+  );
+}
+
+export default App;
